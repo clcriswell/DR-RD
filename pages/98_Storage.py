d8814d69a6c0a090c6cd5359a0404654076214e0
clcriswell <codycriswell@yahoo.com>
2025-09-02 16:00:39 -0700

    Update 98_Storage.py

--- pages/98_Storage.py
+++ pages/98_Storage.py
@@ -5,7 +5,7 @@ from utils import prefs, storage
 st.title("Storage Settings")
 
 conf = prefs.load_prefs().get("storage", {})
-backend = st.selectbox("Backend", ["local", "s3", "gcs"], index=["local", "s3", "gcs"].index(conf.get("backend", "local")))
+backend = st.selectbox("Backend", ["local", "s3", "gcs"], index=["local", "s3", "gcs"].index(conf.get("backend", "gcs")))
 bucket = st.text_input("Bucket", conf.get("bucket", ""))
 prefix = st.text_input("Prefix", conf.get("prefix", "dr_rd"))
 ttl = st.number_input("Signed URL TTL", value=int(conf.get("signed_url_ttl_sec", 600)))
